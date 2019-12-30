ea52166d6ce7a00fbd03bf15116659f1e171bcf0
Archana@me123 <archanaps606@gmail.com>
2019-12-30 11:38:18 +0530

    File for Subtraction

---
+++ subtraction.java
@@ -0,0 +1,17 @@
+public class Subtraction{
+	int a;
+	int b;
+	int sub = 0;
+	
+	//Scanner sc = new Scanner(System.in);
+	System.out.println("Enter first number:");
+	int a = sc.nextInt();
+	System.out.println("Enter second number:");
+	int b = sc.nextInt();
+	
+	public void subtract (int a,int b,){
+		sub = a - b;
+		System.out.println("subtraction :"+ sub);
+		
+	}
+	}
