e9e5764c94b0d555fca9139f56a703082d5060a7
SajinaReshma <sajinaadfo@gmail.com>
2019-12-30 11:33:02 +0530

    file for multiplication

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multiply
+{
+	int num1,num2,product;
+	/*System.out.println("enter the first number: ");
+	Scanner imp=new Scanner(System.in);
+	num1=imp.nextInt();
+	System.out.println("enter the second number: ");
+	num2=imp.nextInt();*/
+	public int prduct(int num1,num2){
+		return num1*num2;
+	}
+}
